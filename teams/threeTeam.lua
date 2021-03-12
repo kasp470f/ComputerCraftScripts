a7d7404db14396691f57dd71f56bd5bdebab86bd
Kasper <kasperwissendorf2011@gmail.com>
2021-03-12 13:51:18 +0100

    Made the threeTeam

--- teams/threeTeam.lua
+++ teams/threeTeam.lua
@@ -1,7 +1,4 @@
-os.setComputerLabel("Digger")
-turtle.refuel()
-
-length = 0
+local length = 0
 
 function mine()
     if turtle.detect() then
@@ -9,41 +6,41 @@ function mine()
     end
     turtle.forward()
 
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnRight()
-    turtle.turnRight()
-    turtle.dig()
-    turtle.turnLeft()
+    turtle.digUp()
+    turtle.digDown()
+    length = length + 1 
 end
 
-function screen()
+function screen() -- Screen reset
     term.clear()
     term.setCursorPos(1,1)
-    print("Fuel Level: " .. turtle.getFuelLevel() .. " meters")
     print("Tunnel Length: " .. length+1)
 end
 
-turtle.forward()
-for i=1,3 do
-    if not turtle.detect() then 
-        turtle.forward() 
-        break 
-    else
-        turtle.up()
+-- Lineup
+while turtle.forward() == false do end
+turtle.turnLeft()
+if turtle.forward() then
+    turtle.turnRight()
+else 
+    turtle.turnRight()
+    turtle.turnRight()
+    if turtle.forward() then
+        turtle.turnLeft()
+    else 
+        turtle.turnLeft()
     end
 end
+term.clear()
+term.setCursorPos(1,1)
+print("Waiting for signal...")
+rednet.open("right")
+rednet.receive()
+turtle.up()
+
 
 while true do
     screen()
-    if turtle.getFuelLevel() < 2 then
-        print("Missing Fuel?!")
-        while turtle.getFuelLevel() < 2 do 
-            turtle.refuel()
-        end
-    end
-    turtle.refuel()
     screen()
     mine()
-    length = length + 1 
 end
